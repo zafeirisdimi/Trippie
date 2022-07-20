2e42eedd0ed0b332b8837b1be6563cacbdfcef44
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-07-20 16:26:57 +0300

    Add project files.

---
+++ GroupProject/Models/Enums/PlaceType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.Models.Enums
+{
+    public enum PlaceType
+    {
+        amusement_park,
+        aquarium,
+        art_gallery,
+        bar,
+        bowling_alley,
+        cafe,
+        campground,
+        casino,
+        church,
+        clothing_store,
+        department_store,
+        hindu_temple,
+        jewelry_store,
+        liquor_store,
+        lodging,
+        meal_delivery,
+        meal_takeaway,
+        mosque,
+        movie_theater,
+        museum,
+        night_club,
+        park,
+        restaurant,
+        rv_park,
+        shoe_store,
+        shopping_mall,
+        spa,
+        stadium,
+        store,
+        supermarket,
+        synagogue,
+        tourist_attraction,
+        zoo
+    }
+}
