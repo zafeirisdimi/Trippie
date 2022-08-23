374b0f5ef709a61af9396a4de1c52f0747e481c6
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-08-23 11:59:00 +0300

    Added the API Controller for the places.

---
+++ GroupProject/Controllers/PlacesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GroupProject.Controllers
+{
+    public class PlacesController : ApiController
+    {
+        [HttpPost]
+        public void GetPlaces(List<object> pathOverview)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
