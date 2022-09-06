0bb3a1edfe7170bb5311ad118dc2b46ece3dd4b1
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-06 13:20:50 +0300

    Added the Trip Controller to the UnregisteredUser area.

---
+++ GroupProject/Areas/UnregisteredUser/UnregisteredUserAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace GroupProject.Areas.UnregisteredUser
+{
+    public class UnregisteredUserAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UnregisteredUser";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UnregisteredUser_default",
+                "UnregisteredUser/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
