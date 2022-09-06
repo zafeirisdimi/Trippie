6d34bf0690e098efa7c4d1e1edf5e4caae045775
Mourikis Leonidas <93220599+MourikisLeonidas@users.noreply.github.com>
2022-09-06 16:11:38 +0300

    Create Point.cs

---
+++ GroupProject/Helpers/Point.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Point
+    {
+        public double Longitude;
+        public double Latitude;
+    }
+}
