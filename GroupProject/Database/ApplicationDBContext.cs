4372f9f03a77476d8e02b3cc2ff31c5f9a178f2b
Mourikis Leonidas <lmourik@hotmail.com>
2022-09-08 15:22:14 +0300

    Connection to DB

---
+++ GroupProject/Database/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using GroupProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.Database
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext() : base("Sindesmos")
+        { }
+        public virtual DbSet<Coordinates> Coordinates { get; set; }
+        public virtual DbSet<CoordinateForReduction> CoordinateForReduction { get; set; }
+        public virtual DbSet<Place> Place { get; set; }
+        public virtual DbSet<Trip> Trip { get; set; }
+    
+    }
+}
---
+++ GroupProject/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace GroupProject.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GroupProject.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(GroupProject.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
