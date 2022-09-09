e6019e35b5cb873f8c583470eecb6dfb23f8208b
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-09 20:22:23 +0300

    Added Pricing Controller.

---
+++ GroupProject/Controllers/PricingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class PricingController : Controller
+    {
+        // GET: Pricing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
