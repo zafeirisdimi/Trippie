37e26d892e2ea3ad776c059a2b5d488370514adf
Mourikis Leonidas <lmourik@hotmail.com>
2022-09-12 21:54:58 +0300

    Changes on connection string

--- GroupProject/Database/ApplicationDBContext.cs
+++ GroupProject/Database/ApplicationDBContext.cs
@@ -7,15 +7,5 @@ using System.Web;
 
 namespace GroupProject.Database
 {
-    public class ApplicationDBContext : DbContext
-    {
-        public ApplicationDBContext() : base("name=MapDBModels")
-        {
-        
-        }
-        public virtual DbSet<Coordinates> Coordinates { get; set; }
-        public virtual DbSet<Location> Location { get; set; }
-        public virtual DbSet<Place> Place { get; set; }
-        public virtual DbSet<Trip> Trip { get; set; }
-    }
+    
 }
--- GroupProject/Models/IdentityModels.cs
+++ GroupProject/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace GroupProject.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MapInitial2", throwIfV1Schema: false)
         {
         }
 
