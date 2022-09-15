d16cd08cd10de057e5bfb5685cd31e6cccfc6167
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-15 10:49:56 +0300

    Implemented CreateTrip action method.

---
+++ GroupProject/Controllers/TripController.cs
@@ -0,0 +1,66 @@
+﻿using GroupProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupProject.Controllers
+{
+    public class TripController : Controller
+    {
+        ApplicationDbContext _context;
+        UserManager<ApplicationUser> _userManager;
+
+        public TripController()
+        {
+            _context = new ApplicationDbContext();
+            var userStore = new UserStore<ApplicationUser>(_context);
+            _userManager = new UserManager<ApplicationUser>(userStore);
+        }
+
+
+        // GET: Trip
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTrip(TripDto dto)
+        {
+            Trip trip = new Trip(dto);
+
+            var userId = User.Identity.GetUserId();
+            var user = _userManager.FindById(userId);
+
+            // Add null checks for the unregistered user
+            // if user ==  null do something else
+
+            trip.ApplicationUser = user;
+
+            _context.Trips.Add(trip);
+
+            _context.SaveChanges();
+
+            List<PlaceType> placeTypes = new List<PlaceType>();
+
+            PlaceType placeType;
+
+            foreach (var value in dto.ChosenPlaceTypes)
+            {
+                placeType = _context.PlaceTypes.Find(value);
+
+                placeTypes.Add(placeType);
+            }
+
+            trip.ChosenPlaceTypes = placeTypes;
+
+            _context.SaveChanges();
+
+            return View();
+        }
+    }
+}
