7037f4fd6be5941447ac3e6bb4e8c436390462f8
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-17 14:23:50 +0300

    Added ViewModel for the admin view.

--- GroupProject/Controllers/AdminController.cs
+++ GroupProject/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GroupProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +11,55 @@ namespace GroupProject.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
+
+
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+            var users = GetUsers().ToList();
+            var trips = GetTrips().ToList();
+            var places = GetPlaces().ToList();
+            var cities = GetCities().ToList();
+
+            var viewModel = new AdminViewModel
+            {
+                Users = users,
+                Trips = trips,
+                Places = places,
+                Cities = cities
+            };
+
+
+            return View(viewModel);
         }
+
+
+        [NonAction]
+        public IEnumerable<ApplicationUser> GetUsers()
+        {
+            return _context.Users;
+        }
+
+        [NonAction]
+        public IEnumerable<Trip> GetTrips()
+        {
+            return _context.Trips;
+        }
+         
+        [NonAction]
+        public IEnumerable<Place> GetPlaces()
+        {
+            return _context.Places;
+        }
+
+        [NonAction]
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities;
+        }
+
+
+
     }
 }
---
+++ GroupProject/Models/AdminViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject.Models
+{
+    public class AdminViewModel
+    {
+        public List<ApplicationUser> Users { get; set; }
+        public List<City> Cities { get; set; }
+        public List<Place> Places { get; set; }
+        public List<Trip> Trips { get; set; }
+    }
+}
