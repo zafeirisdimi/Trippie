fac5566a013a3a6348ff93882b781d0c668dda61
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-18 14:10:20 +0300

    Added values on the sliders.

---
+++ GroupProject/Scripts/trip/sliders.js
@@ -0,0 +1,15 @@
+﻿let radiusInput = document.querySelector('input[name=radius]');
+let radiusValue = document.querySelector('input[name=radius] ~ p');
+radiusValue.textContent = radiusInput.value;
+
+radiusInput.addEventListener('input', () => {
+    radiusValue.textContent = radiusInput.value;
+});
+
+let pointsInput = document.querySelector('input[name=points]');
+let pointsValue = document.querySelector('input[name=points] ~ p');
+pointsValue.textContent = pointsInput.value;
+
+pointsInput.addEventListener('input', () => {
+    pointsValue.textContent = pointsInput.value;
+});
