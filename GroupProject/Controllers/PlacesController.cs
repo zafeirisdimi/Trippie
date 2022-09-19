e37c07ef366ad3c6efc761cc00bdb2d8c5498abc
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-19 12:49:24 +0300

    Moved some code to new methods.

--- GroupProject/Controllers/PlacesController.cs
+++ GroupProject/Controllers/PlacesController.cs
@@ -14,6 +14,8 @@ using System.Collections.Concurrent;
 using System.Web.Http.Cors;
 using System.Threading;
 using GroupProject.Models.Dtos;
+using GroupProject.Models.Helper;
+using GroupProject.Models.Entities;
 
 namespace GroupProject.Controllers
 {
@@ -22,23 +24,53 @@ namespace GroupProject.Controllers
     public class PlacesController : ApiController
     {
         private static readonly HttpClient _client = new HttpClient();
-        private string apiKey = "5ae2e3f221c38a28845f05b6068096737a6bd1b9a215367ca1d1bd33";
+        private readonly string baseUri = "https://api.opentripmap.com/0.1/en/places/radius?";
+        private readonly string apiKey = "5ae2e3f221c38a28845f05b6068096737a6bd1b9a215367ca1d1bd33";
 
 
         [HttpPost]
         public async Task<IEnumerable<PlaceDtoForOpenTripMap>> GetPlacesAlongPath(SearchAlongPathDto dto)
         {
+            SetDecimalSeparator();
+
             int points = dto.PointsAlongPath ?? 8;
             int radius = dto.Radius ?? 7000;
 
-
             var reducedPath = PointReductionHelper.ReducePoints(dto.PathOverview, points);
 
-            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
-            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            var places = await GetPlaces(reducedPath, dto.PlaceTypes, radius);
 
-            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            return places.GroupBy(p => p.xid)
+                               .Select(g => g.FirstOrDefault());
+        }
 
+        [NonAction]
+        public Uri RadiusEndpoint(int radius, double lon, double lat, PlaceTypeEnum[] placeTypes, int rate = 2, int limit = 50, string format = "json")
+        {
+            var typesParsed = placeTypes.Select(value => Enum.GetName(typeof(PlaceTypeEnum), value))
+                                 .Select(name => char.ToLower(name[0]).ToString() + name.Substring(1))
+                                 .ToArray();
+
+            var kinds = String.Join(",", typesParsed);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(baseUri)
+              .Append($"radius={radius}")
+              .Append($"&lon={lon}")
+              .Append($"&lat={lat}")
+              .Append($"&kinds={kinds}")
+              .Append($"&rate={rate}")
+              .Append($"&limit={limit}")
+              .Append($"&format={format}")
+              .Append($"&apikey={apiKey}");
+
+            return new Uri(sb.ToString());
+        }
+
+        [NonAction]
+        public async Task<List<PlaceDtoForOpenTripMap>> GetPlaces(Coordinates[] reducedPath, PlaceTypeEnum[] placeTypes, int radius)
+        {
             ConcurrentBag<PlaceDtoForOpenTripMap> placesConcur = new ConcurrentBag<PlaceDtoForOpenTripMap>();
 
             var tasks = new List<Task<PlaceDtoForOpenTripMap[]>>();
@@ -56,7 +88,7 @@ namespace GroupProject.Controllers
 
                 for (int j = 0; j < currentCoordinates.Count; j++)
                 {
-                    placesRadiusAPI = RadiusEndpoint(radius, currentCoordinates[j].Longitude, currentCoordinates[j].Latitude, dto.PlaceTypes, 3);
+                    placesRadiusAPI = RadiusEndpoint(radius, currentCoordinates[j].Longitude, currentCoordinates[j].Latitude, placeTypes, 3);
                     tasks.Add(_client.GetFromJsonAsync<PlaceDtoForOpenTripMap[]>(placesRadiusAPI));
                 }
 
@@ -71,41 +103,20 @@ namespace GroupProject.Controllers
 
                 if (i < numberOfBatches - 1)
                     await Task.Delay(2000);
-                
+
                 tasks.Clear();
             }
 
-            return placesConcur.GroupBy(p => p.xid)
-                               .Select(g => g.FirstOrDefault());
+            return placesConcur.ToList();
         }
 
         [NonAction]
-        public Uri RadiusEndpoint(int radius, double lon, double lat, PlaceTypeEnum[] placeTypes, int rate = 2, int limit = 50, string format = "json")
+        public void SetDecimalSeparator()
         {
-            var test = placeTypes.Select(value => Enum.GetName(typeof(PlaceTypeEnum), value))
-                                 .Select(name => char.ToLower(name[0]).ToString() + name.Substring(1))
-                                 .ToArray();
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
 
-
-            var kinds = String.Join(",", test);
-                //"historic,natural"
-
-
-            StringBuilder sb = new StringBuilder();
-
-            var baseUri = "https://api.opentripmap.com/0.1/en/places/radius?";
-
-            sb.Append(baseUri)
-              .Append($"radius={radius}")
-              .Append($"&lon={lon}")
-              .Append($"&lat={lat}")
-              .Append($"&kinds={kinds}")
-              .Append($"&rate={rate}")
-              .Append($"&limit={limit}")
-              .Append($"&format={format}")
-              .Append($"&apikey={apiKey}");
-
-            return new Uri(sb.ToString());
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
         }
 
     }
