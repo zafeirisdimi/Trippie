54f1e7a6578e2551f0b9d642a4d6a7ad1e0756e3
GianThyr <64744759+GianThyr@users.noreply.github.com>
2022-09-23 00:34:12 +0300

    Implemented the basic chat feature.

---
+++ GroupProject/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message, string roleTest)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message, roleTest);
+        }
+
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
--- GroupProject/Startup.cs
+++ GroupProject/Startup.cs
@@ -9,6 +9,7 @@ namespace GroupProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
